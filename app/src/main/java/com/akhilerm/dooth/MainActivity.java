5de9a5cce1b39d75a0fa3d334b9e68188fe4d860
Akhil Mohan <akhilerm@gmail.com>
2018-04-21 12:42:02 +0400

    Initial Commit

---
+++ app/src/main/java/com/akhilerm/dooth/MainActivity.java
@@ -0,0 +1,125 @@
+package com.akhilerm.dooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.amazon.identity.auth.device.AuthError;
+import com.amazon.identity.auth.device.api.Listener;
+import com.amazon.identity.auth.device.api.authorization.AuthCancellation;
+import com.amazon.identity.auth.device.api.authorization.AuthorizationManager;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeListener;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeRequest;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeResult;
+import com.amazon.identity.auth.device.api.authorization.ProfileScope;
+import com.amazon.identity.auth.device.api.authorization.Scope;
+import com.amazon.identity.auth.device.api.authorization.User;
+import com.amazon.identity.auth.device.api.workflow.RequestContext;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestContext requestContext;
+    private String name;
+    private String email;
+    private String account;
+    private String zipcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestContext = RequestContext.create(this);
+        requestContext.registerListener(new AuthorizeListener() {
+            @Override
+            public void onSuccess(AuthorizeResult authorizeResult) {
+                fetchUserProfile();
+            }
+
+            @Override
+            public void onError(AuthError authError) {
+                Toast.makeText(MainActivity.this, "Auth Error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel(AuthCancellation authCancellation) {
+                Toast.makeText(MainActivity.this, "Auth Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton loginButton = findViewById(R.id.login_with_amazon);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthorizationManager.authorize(new AuthorizeRequest
+                        .Builder(requestContext)
+                        .addScopes(ProfileScope.profile(), ProfileScope.postalCode())
+                        .build());
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestContext.onResume();
+    }
+
+    private void fetchUserProfile() {
+        User.fetch(this, new Listener<User,AuthError>() {
+
+            /* fetch completed successfully. */
+            @Override
+            public void onSuccess(User user) {
+                final String name = user.getUserName();
+                final String email = user.getUserEmail();
+                final String account = user.getUserId();
+                final String zipcode = user.getUserPostalCode();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateProfileData(name, email, account, zipcode);
+                    }
+                });
+            }
+
+            /* There was an error during the attempt to get the profile. */
+            @Override
+            public void onError(AuthError ae) {
+                /* Retry or inform the user of the error */
+            }
+        });
+    }
+
+    private synchronized void updateProfileData(String name, String email, String account, String zipcode) {
+        this.name=name;
+        this.email=email;
+        this.account=account;
+        this.zipcode=zipcode;
+    }
+
+    @Override
+    protected void onStart(){ super.onStart();
+        Scope[] scopes = { ProfileScope.profile(), ProfileScope.postalCode() };
+        AuthorizationManager.getToken(this, scopes, new Listener<AuthorizeResult, AuthError>() {
+
+            @Override
+            public void onSuccess(AuthorizeResult result) {
+                if (result.getAccessToken() != null) {
+                    /* The user is signed in */
+                } else {
+                    /* The user is not signed in */
+                }
+            }
+
+            @Override
+            public void onError(AuthError ae) {
+                /* The user is not signed in */
+            }
+        });
+    }
+
+
+}
